53887e2093af1b621dd36481cd9c4ca574961e3b
Lucas Sta Maria <lucas@priime.dev>
2024-01-10 03:07:18 -0500

    fix: add support for log formatting
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

--- info.rkt
+++ info.rkt
@@ -5,6 +5,6 @@
 (define build-deps '("scribble-lib"))
 (define scribblings '(("scribblings/logman.scrbl" (multi-page))))
 (define pkg-desc "Small logging improvements")
-(define version "0.1")
+(define version "0.2")
 (define license 'MIT)
 (define pkg-authors '(priime0))
--- main.rkt
+++ main.rkt
@@ -19,17 +19,37 @@
   (define time+ (date->string (current-date) (current-seconds)))
   (format "~a ~a - ~a" level-text time+ message))
 
-(define (log-debug message)
-  (rkt:log-debug (log-format 'debug message)))
+(define-syntax log-debug
+  (syntax-rules ()
+    [(_ message)
+     (rkt:log-debug (log-format 'debug message))]
+    [(log-debug format-string-expr v ...)
+     (rkt:log-debug (log-format 'debug (format format-string-expr v ...)))]))
 
-(define (log-info message)
-  (rkt:log-info (log-format 'info message)))
+(define-syntax log-info
+  (syntax-rules ()
+    [(_ message)
+     (rkt:log-info (log-format 'info message))]
+    [(_ format-string-expr v ...)
+     (rkt:log-info (log-format 'info (format format-string-expr v ...)))]))
 
-(define (log-warning message)
-  (rkt:log-warning (log-format 'warn message)))
+(define-syntax log-warning
+  (syntax-rules ()
+    [(_ message)
+     (rkt:log-warning (log-format 'warn message))]
+    [(_ format-string-expr v ...)
+     (rkt:log-warning (log-format 'warn (format format-string-expr v ...)))]))
 
-(define (log-error message)
-  (rkt:log-error (log-format 'error message)))
+(define-syntax log-error
+  (syntax-rules ()
+    [(_ message)
+     (rkt:log-error (log-format 'error message))]
+    [(_ format-string-expr v ...)
+     (rkt:log-error (log-format 'error (format format-string-expr v ...)))]))
 
-(define (log-fatal message)
-  (rkt:log-fatal (log-format 'fatal message)))
+(define-syntax log-fatal
+  (syntax-rules ()
+    [(_ message)
+     (rkt:log-fatal (log-format 'fatal message))]
+    [(_ format-string-expr v ...)
+     (rkt:log-fatal (log-format 'fatal (format format-string-expr v ...)))]))
