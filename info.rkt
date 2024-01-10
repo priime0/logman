51f0c7221d91e130e4ce29298324a1f59417c66e
Lucas Sta Maria <lucas@priime.dev>
2024-01-10 02:46:00 -0500

    chore: add base project
    Signed-off-by: Lucas Sta Maria <lucas@priime.dev>

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection "logman")
+(define deps '("base"))
+(define build-deps '("scribble-lib"))
+(define scribblings '(("scribblings/logman.scrbl" (multi-page))))
+(define pkg-desc "Small logging improvements")
+(define version "0.1")
+(define license 'MIT)
+(define pkg-authors '(priime0))
